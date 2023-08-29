5b440710118d951b366d09b34ebe7bdef7e1959e
armanpinachyan <pinachyan99@mail.ru>
2023-08-29 18:10:50 +0400

    18:10

--- js/script.js
+++ js/script.js
@@ -118,3 +118,170 @@ $('.gallery-photo').on('click', function (e) {
     new PhotoViewer(items, options);
 
 });
+
+
+// Modal-info img
+
+const AllImages = [
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider1.png',
+      'assets/image/slider2.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider3.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider4.png',
+      'assets/image/slider5.png'
+    ]
+  },
+  // row
+  {
+    class: 'col-md-8',
+    image: [
+      'assets/image/slider6.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider7.png'
+    ]
+  },
+  // row
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider8.png'
+    ]
+  },
+  {
+    class: 'col-md-8',
+    image: [
+      'assets/image/slider10.png'
+    ]
+  },
+  // row
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider11.png',
+      'assets/image/slider12.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider13.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider14.png',
+      'assets/image/slider16.png',
+    ]
+  },
+  // row
+  {
+    class: 'col-md-8',
+    image: [
+      'assets/image/slider17.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider18.png'
+    ]
+  },
+  // row
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider19.png'
+    ]
+  },
+  {
+    class: 'col-md-8',
+    image: [
+      'assets/image/slider20.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider21.png'
+    ]
+  },
+  {
+    class: 'col-md-8',
+    image: [
+      'assets/image/slider22.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider23.png',
+      'assets/image/slider24.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider25.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider26.png',
+      'assets/image/slider27.png',
+    ]
+  },
+  {
+    class: 'col-md-8',
+    image: [
+      'assets/image/slider30.png'
+    ]
+  },
+  {
+    class: 'col-md-4',
+    image: [
+      'assets/image/slider29.png'
+    ]
+  },
+  // row
+];
+
+const myModal = new bootstrap.Modal('#info-block', {
+  keyboard: false
+})
+
+// #info-block
+
+document.getElementById('btn-open-ll-works').addEventListener('click', function (){
+  myModal.show()
+    AllImages.forEach((img) => {
+        document.getElementById('row-modal-info').insertAdjacentHTML('beforeend', `
+          <div class="${img.class} modal-images-col">
+              <div class="modal-images-item">
+                ${printImages(img.image).join('')}
+              </div>
+          </div>
+        `)
+    })
+})
+
+function printImages(imgArray){
+  return imgArray.map((imgUrl) => `<img src="${imgUrl}" alt="mod" style="object-fit: cover; height: ${imgArray.length > 1 ? ` calc((100% / ${imgArray.length}) - 10px)` : '100%'}" >`)
+}
